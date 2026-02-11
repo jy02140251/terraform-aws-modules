b256dbfb46c2124d8d34c214d0909135124b67f6
Wu Xie <153907200+jy02140251@users.noreply.github.com>
2026-02-12 02:33:01 +0800

    Add template

---
+++ modules/ec2/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "WebServer"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.web.public_ip
+}
